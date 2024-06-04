c488cc8d3a43532bd719300d82d0d828dfa9c242
yoozeze <fbghrud@naver.com>
2024-06-04 18:10:20 +0900

    240604_유호경_nav

--- HanJan/resources/components/AppComponent.vue
+++ HanJan/resources/components/AppComponent.vue
@@ -1,15 +1,64 @@
 <template>
     <!-- header -->
     <header>
-        <div class="header-container">
-            <div class="header-content">
-                
+        <div class="header_container">
+            <div class="header_content">
+                <div class="logo"><a href=""><img src="/img/logo.png" class="logo"></a></div>
+                <ul class="nav">
+                    <li class="nav_box">
+                        <a href="" class="nav_font nav_title">
+                            <ul class="nav_mini">
+                                <li class="nav_mini_hover">전체 메뉴</li>
+                                <li>
+                                    <ul class="nav_mini_title">
+                                        <a href="" class="nav_font"><li>탁주</li></a>
+                                        <a href="" class="nav_font"><li>과실주</li></a>
+                                        <a href="" class="nav_font"><li>증류주</li></a>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </a>
+                    </li>
+                    <li class="nav_box">
+                        <a href="" class="nav_font nav_title">
+                            전통주 이야기
+                        </a>
+                    </li>
+                    <li class="nav_box">
+                        <a href="" class="nav_font nav_title">
+                            1 : 1 문의
+                        </a>
+                    </li>
+                    <li class="nav_box">
+                        <a href=""  class="nav_font nav_title">
+                            공지사항
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" class="nav_font nav_user_box">
+                            <img src="/img/login.png" class="nav_icon">
+                            <div class="nav_user">로그인</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" class="nav_font nav_user_box">
+                            <img src="/img/regist.png" class="nav_icon">
+                            <div class="nav_user">회원가입</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" class="nav_font nav_user_box">
+                            <img src="/img/bag.png" class="nav_icon">
+                            <div class="nav_user">장바구니</div>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </header>
 </template>
 
-<script></script>
+<script setup></script>
 
 <style>
     @import url('../css/common.css');
--- HanJan/resources/views/welcome.blade.php
+++ HanJan/resources/views/welcome.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HANJAN</title>
-</head>
-<body>
-    <div id="app">
-        <App-Component></App-Component>
-    </div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <title>HANJAN</title>
+    </head>
+    <body>
+        <div id="app">
+            <App-Component></App-Component>
+        </div>
+    </body>
 </html>
