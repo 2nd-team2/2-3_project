32d4e15587829360c5c459b4b838fd97943c8355
yoozeze <fbghrud@naver.com>
2024-06-04 12:52:06 +0900

    240604_유호경_main

---
+++ HanJan/resources/components/AppComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- header -->
+    <header>
+        <div class="header-container">
+            <div class="header-content">
+                
+            </div>
+        </div>
+    </header>
+</template>
+
+<script></script>
+
+<style>
+    @import url('../css/common.css');
+</style>
