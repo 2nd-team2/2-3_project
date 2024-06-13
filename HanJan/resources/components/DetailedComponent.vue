f61d0d5c1fd79135f42ba3f89eca6efe2d06bb5e
minsea1256 <minsea1256@gmail.com>
2024-06-13 16:36:59 +0900

    권민서_수량체크완료_0613

--- HanJan/resources/components/DetailedComponent.vue
+++ HanJan/resources/components/DetailedComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-    <form action="" method="get">
+    <!-- <form> -->
         <div class="detailed_haeder">
             <img src="/img/best.png">
             <div class="detailed_haeder_item">
@@ -13,25 +13,30 @@
                     <p>5.0</p>
                     <p class="detailed_review">리뷰 999</p>
                 </div>
-                <p>수량</p>
+                <p id="app">수량</p>
                 <div class="detailed_quantity">
-                    <button class="detailed_haeder_plus" type="submit">-</button>
-                    <p>{{ quantity }}</p>
-                    <button class="detailed_haeder_height" type="submit">+</button>
+                    <button @click="decrement" class="detailed_haeder_minus" type="submit">-</button>
+                    <p id="quantityDisplay" class="detailed_haeder_quantity">{{ quantity }}</p>
+                    <button @click="increment" class="detailed_haeder_plus" type="submit">+</button>
                 </div>
                 <p>총 상품가격</p>
                 <p class="detailed_haeder_num">24,000원</p>
+                
                 <div class="detailed_haeder_btn">
-                    <a @mouseover="openIconBag" @mouseleave="closeIconBag" class="detailed_haeder_bag_a" href="/bag">
-                        <img src="/img/bag.png" class="detailed_haeder_bag_w" id="b_detailed">
-                        <img src="/img/bag_b.png" class="detailed_haeder_bag_b" id="bk_detailed">
-                        <div @click.prevent="zeroAlert" class="detailed_haeder_bag">장바구니</div>
-                    </a>
-                    <button type="submit" @click="zeroAlert" class="detailed_haeder_bay">구매하기</button>
+                    <router-link to="/bag">
+                        <button type="submit" @mouseover="openIconBag" @mouseleave="closeIconBag" @click="zeroAlert" class="detailed_haeder_bag_a">
+                            <img src="/img/bag.png" class="detailed_haeder_bag_w" id="b_detailed">
+                            <img src="/img/bag_b.png" class="detailed_haeder_bag_b" id="bk_detailed">
+                            <div class="detailed_haeder_bag">장바구니</div>
+                        </button>
+                    </router-link>
+                    <router-link to="/order">
+                        <button type="submit" @click="zeroAlert" class="detailed_haeder_bay">구매하기</button>
+                    </router-link>
                 </div>
             </div>
         </div>
-    </form>
+    <!-- </form> -->
     <div class="detailed_content">
         <img src="/img/detailed_content.png">
     </div>
@@ -140,17 +145,28 @@
 </template>
 
 <script setup>
-// 재고수량 0일때 alert 띄우기
-const quantity = 0; // 초기 수량
-function zeroAlert(event) {
-    event.preventDefault(); // 기본 동작(페이지 이동)을 막습니다.
-        if( quantity = 0 ) {
-            quantity;
-        
-        }else {
-            alert('수량이 0이므로 확인 부탁드립니다.');
+    import { ref } from 'vue';
+    const quantity = ref(0); //처음 숫자지정
+
+    // 재고수량 0일때 alert 띄우기
+    const zeroAlert = () => {
+        if ( quantity.value <= 0 ) {
+            alert('수량을 1 이상으로 선택해주세요.');
+            event.preventDefault();
         }
-    }
+    };
+    // + 버튼을 클릭했을 때 실행되는 함수
+    const increment = () => {
+        quantity.value ++;
+    };
+
+    // - 버튼을 클릭했을 때 실행되는 함수
+    const decrement = () => {
+        if (quantity.value > 0) {
+            quantity.value--;
+        }
+    };
+
     // 아이콘 호버시 색 변환
     function openIconBag() {
         const g_iconbag = document.querySelector('#bk_detailed');
@@ -166,18 +182,6 @@ function zeroAlert(event) {
         g_iconbag.classList.add('detailed_haeder_bag_b');
         b_iconbag.style.display = 'block';
     }
-
-    // 재고수량 0일때 alert 띄우기
-    // let quantity = 0; // 초기 수량을 0으로 설정합니다.
-
-    // function decreaseQuantity() {
-    //     if (quantity > 0) {
-    //         // quantity.value--; = quantity의 값을 1만큼 감소 자동으로 값 업데이트 해준다
-    //         quantity --;
-    //     } else {
-    //         alert("수량이 0이므로 다시확인 부탁드립니다.");
-    //     }
-    // }
 </script>
 
 <style scoped src="../css/detailed.css">
--- HanJan/resources/components/ListComponent.vue
+++ HanJan/resources/components/ListComponent.vue
@@ -3,24 +3,24 @@
         <div class="list_main_img" style="background-image: url('/img/list_img03.png');"></div>
         <div class="list_menu">
             <router-link to="/list">
-                <a class="list_menu_img">
+                <button type="submit" class="list_menu_img">
                     <img src="/img/menu01.png" alt="탁주">
                     <p>탁주</p>
-                </a>
+                </button>
             </router-link>
             <div class="list_line"></div>
             <router-link to="/list">
-                <a class="list_menu_img">
+                <button type="submit" class="list_menu_img">
                     <img src="/img/menu02.png" alt="과실주">
                     <p>과실주</p>
-                </a>
+                </button>
             </router-link>
             <div class="list_line"></div>
             <router-link to="/list">
-                <a class="list_menu_img">
+                <button type="submit" class="list_menu_img">
                     <img src="/img/menu03.png" alt="중류주">
                     <p>중류주</p>
-                </a>
+                </button>
             </router-link>
         </div>
         <p class="list_best_title">한잔 베스트</p>
