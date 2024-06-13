4242a6b3970f4615c98bfca70c5fd76e0032f7a3
minsea1256 <minsea1256@gmail.com>
2024-06-13 11:35:33 +0900

    권민서_이미지변경_0613

--- HanJan/resources/components/DetailedComponent.vue
+++ HanJan/resources/components/DetailedComponent.vue
@@ -1,36 +1,39 @@
 <template>
     <main>
-    <div class="detailed_haeder">
-        <img src="/img/best.png">
-        <div class="detailed_haeder_item">
-            <p>적당한 산미와 쌀의 감칠맛</p>
-            <p class="detailed_haeder_title">꿈의 대화</p>
-            <p>판매가격 :</p>
-            <p class="detailed_haeder_price">24,000원</p>
-            <div class="detailed_haeder_review">
-                <img src="/img/detailed_star.png">
-                <p>5.0</p>
-                <p class="detailed_review">리뷰 999</p>
-            </div>
-            <p>수량</p>
-            <div class="detailed_quantity">
-                <button class="detailed_haeder_plus" type="submit">-</button>
-                <p class="detailed_haeder_quantity">0</p>
-                <button class="detailed_haeder_height" type="submit">+</button>
-            </div>
-            <p>총 상품가격</p>
-            <p class="detailed_haeder_num">24,000원</p>
-            <div class="detailed_haeder_btn">
-                <a href="/bag">
-                    <img src="/img/bag.png">
-                    <!-- TODO -->
-                    <img src="/img/bag_b.gnh"> 
-                    장바구니
-                </a>
-                <button type="submit" @click="zeroAlert" class="detailed_haeder_b">구매하기</button>
+    <form action="" method="get">
+        <div class="detailed_haeder">
+            <img src="/img/best.png">
+            <div class="detailed_haeder_item">
+                <p>적당한 산미와 쌀의 감칠맛</p>
+                <p class="detailed_haeder_title">꿈의 대화</p>
+                <p>판매가격 :</p>
+                <p class="detailed_haeder_price">24,000원</p>
+                <div class="detailed_haeder_review">
+                    <img src="/img/detailed_star.png">
+                    <p>5.0</p>
+                    <p class="detailed_review">리뷰 999</p>
+                </div>
+                <label for="">수량</label>
+                <input type="number" name="" id="">
+                <p>수량</p>
+                <div class="detailed_quantity">
+                    <button class="detailed_haeder_plus" type="submit">-</button>
+                    <p class="detailed_haeder_quantity">0</p>
+                    <button class="detailed_haeder_height" type="submit">+</button>
+                </div>
+                <p>총 상품가격</p>
+                <p class="detailed_haeder_num">24,000원</p>
+                <div class="detailed_haeder_btn">
+                    <a @mouseover="openIconBag" @mouseleave="closeIconBag" class="detailed_haeder_bag_a" href="/bag">
+                        <img src="/img/bag.png" class="detailed_haeder_bag_w" id="b_detailed">
+                        <img src="/img/bag_b.png" class="detailed_haeder_bag_b" id="bk_detailed">
+                        <div class="detailed_haeder_bag">장바구니</div>
+                    </a>
+                    <button type="submit" @click="zeroAlert" class="detailed_haeder_bay">구매하기</button>
+                </div>
             </div>
         </div>
-    </div>
+    </form>
     <div class="detailed_content">
         <img src="/img/detailed_content.png">
     </div>
@@ -138,9 +141,25 @@
 </template>
 
 <script setup>
-// alert 띄우기
+    // alert 띄우기
     function zeroAlert() {
         alert('수량확인 부탁드립니다.');
+    }
+
+    // 아이콘 호버시 색 변환
+    function openIconBag() {
+        const g_iconbag = document.querySelector('#bk_detailed');
+        const b_iconbag = document.querySelector('#b_detailed');
+
+        g_iconbag.classList.remove('detailed_haeder_bag_b');
+        b_iconbag.style.display = 'none';
+    }
+    function closeIconBag() {
+        const g_iconbag = document.querySelector('#bk_detailed');
+        const b_iconbag = document.querySelector('#b_detailed');
+
+        g_iconbag.classList.add('detailed_haeder_bag_b');
+        b_iconbag.style.display = 'block';
     }
 </script>
 
