921e437e7a7a633f15ce4637ec543b1b52077468
yoozeze <fbghrud3@nver.com>
2024-06-25 14:42:11 +0900

    240625_유호경_리스트스크롤수정

--- HanJan/resources/components/OrderCompleteComponent.vue
+++ HanJan/resources/components/OrderCompleteComponent.vue
@@ -1,17 +1,30 @@
 <template>
     <main>
-        <div>
-            주문이 완료 되었습니다.
+        <div class="header_top">
+            <ul class="header_step">
+                <h2>결제 완료</h2>
+                <li>01 장바구니</li>
+                <li>></li>
+                <li>02 정보입력</li>
+                <li>></li>
+                <li class="li_yellow">03 결제완료</li>
+            </ul>
         </div>
-        <router-link to="/list?type=99&page=1" class="bag_cancel">
-            계속 쇼핑하기
-        </router-link>
+        <div class="ord_com">
+            <div class="ord_com_box">
+                <h2>
+                    주문이 완료 되었습니다.
+                </h2>
+                <router-link to="/list?type=99&page=1" class="bag_cancel">
+                    계속 쇼핑하기
+                </router-link>
+            </div>
+        </div>
     </main>
 </template>
 
 <script setup>
 </script>
 
-<style scoped src="../css/order.css">
-/* css 수정하려면 src 수정도 같이하기 */
+<style scoped src="../css/ordercom.css">
 </style>
