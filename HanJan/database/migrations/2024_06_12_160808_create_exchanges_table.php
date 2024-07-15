58cab41d0a715f786bce6ad4269580730c21ddcc
BOW-ON <qhdnjs94@gmail.com>
2024-07-15 11:35:07 +0900

    240715 서보원 데이터 베이스 리셋

--- HanJan/database/migrations/2024_06_12_160808_create_exchanges_table.php
+++ HanJan/database/migrations/2024_06_12_160808_create_exchanges_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('ex_det_addr', 100)->nullable()->comment('상품회수지상세');
             $table->integer('ex_post')->nullable()->comment('상품회수지 우편번호');
             $table->char('ex_reason', 1)->nullable()->comment('사유, 0: 단순변심, 1:상품배송오류, 2:구성품부족, 3:파손,결함, 4:기타');
-            $table->char('ex_reason_etc', 500)->nullable()->comment('사유, 기타');
+            $table->string('ex_reason_etc', 500)->nullable()->comment('사유, 기타');
             $table->char('ex_flg', 1)->default(3)->comment('교환 flg, 0:신청완료, 1:상품회수중 2:처리완료 3:미신청');
             // $table->timestamp('created_at')->useCurrent();
             $table->timestamps();
