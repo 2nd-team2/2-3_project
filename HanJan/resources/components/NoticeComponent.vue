d76c464ab6b5516e518c0add20498f9790fbdb63
BOW-ON <qhdnjs94@gmail.com>
2024-07-15 16:33:41 +0900

    240715 서보원 공지 줄바꿈 수정 완료

--- HanJan/resources/components/NoticeComponent.vue
+++ HanJan/resources/components/NoticeComponent.vue
@@ -6,13 +6,30 @@
             <div class="notice_line"></div>
             <div class="notice_name">제목</div>
             <div class="notice_item">{{ $store.state.noticeDetail.no_title }}</div>
-            <div class="notice_item_content">{{ $store.state.noticeDetail.no_content }}</div>
+            <!-- <div class="notice_item_content">{{ $store.state.noticeDetail.no_content }}</div> -->
+            <div class="notice_item_content" v-html="formattedContent"></div>
+            
             <div class="notice_item_date">{{ $store.state.noticeDetail.created_at }}</div>
         </div>
     </main>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+// 데이터에서 content 가져오기
+const noticeContent = computed(() => store.state.noticeDetail.no_content);
+
+// 줄바꿈을 <br> 태그로 변환하여 HTML로 표시
+const formattedContent = computed(() => {
+    if (noticeContent.value) {
+        return noticeContent.value.replace(/\n/g, '<br>');
+    }
+    return '';
+});
 </script>
 
 <style>
