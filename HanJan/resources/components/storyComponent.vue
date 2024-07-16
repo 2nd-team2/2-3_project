bc044f50ce35d2089857eca7f86d98acbaf2144b
minsea1256 <minsea1256@gmail.com>
2024-07-16 10:59:29 +0900

    권민서_한잔이야기 뒤로가기_0716

--- HanJan/resources/components/storyComponent.vue
+++ HanJan/resources/components/storyComponent.vue
@@ -2,9 +2,17 @@
     <div class="story_list">
         <img src="/img/story_list.png">
     </div>
+    <div class="bk_btn" @click="goBack">뒤로가기</div>
 </template>
 
 <script setup>
+    import { useRouter } from 'vue-router'
+
+    const router = useRouter()
+
+    const goBack = () => {
+        router.back();
+    }
 </script>
 
 <style scoped src="../css/story.css">
