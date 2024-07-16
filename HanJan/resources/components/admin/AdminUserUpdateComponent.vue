27375b6ba06a3354b3817af0f2e81b4263d97419
yoozeze <fbghrud3@nver.com>
2024-07-16 11:40:00 +0900

    240716_유호경_회원수정비밀번호추가 등

--- HanJan/resources/components/admin/AdminUserUpdateComponent.vue
+++ HanJan/resources/components/admin/AdminUserUpdateComponent.vue
@@ -7,24 +7,32 @@
                 <label for="name" class="admin_user_info0">유저 이름</label>
                 <p class="admin_error">{{ nameError }}</p>
                 <input @input="chkName" type="text" name="name" id="name" autocomplete='off' v-model="name" class="admin_user_info1">
+
                 <label for="email" class="admin_user_info2">이메일</label>
                 <p class="admin_error">{{ emailError }}</p>
                 <input @input="chkEmail" type="email" name="email" id="email" autocomplete='off' v-model="email" class="admin_user_info3">
                 <button type="button" class="admin_btn admin_user_info14" @click="$store.dispatch('adminChkEmailOn', emailText)">이메일 중복확인</button>
-                <label for="tel" class="admin_user_info4">휴대전화</label>
+
+                <label for="password" class="admin_user_info5">비밀번호 수정</label>
+                <p class="admin_error">{{ passwordError }}</p>
+                <input type="password" v-model="password" name="password" id="password" @input="chkPassword" class="admin_user_info6">
+
+                <label for="tel" class="admin_user_info7">휴대전화</label>
                 <p class="admin_error">{{ phoneError }}</p>
-                <input @input="chkPhone" type="text" name="tel" id="tel" autocomplete='off' placeholder="-를 제외한 숫자만 입력해주세요" v-model="tel" class="admin_user_info5">
-                <label for="addr" class="admin_user_info6">주소</label>
+                <input @input="chkPhone" type="text" name="tel" id="tel" autocomplete='off' placeholder="-를 제외한 숫자만 입력해주세요" v-model="tel" class="admin_user_info8">
+
+                <label for="addr" class="admin_user_info9">주소</label>
                 <p class="admin_error">{{ addressError }}</p>
-                <input @input="chkAddress" readonly type="text" name="addr" id="addr" autocomplete='off' v-model="address" @click="kakaoPostcode" class="admin_user_info7">
-                <label for="det_addr" class="admin_user_info8">상세 주소</label>
-                <input type="text" name="det_addr" id="det_addr" autocomplete='off' v-model="detAddr" class="admin_user_info9">
-                <label for="post" class="admin_user_info10">우편번호</label>
-                <input readonly type="text" name="post" id="post" autocomplete='off' v-model="post" class="admin_user_info11">
-                <button type="button" @click="kakaoPostcode" id="postcode" class="admin_btn admin_user_info7">주소검색</button>
-                <label for="birth" class="admin_user_info12">생년월일</label>
+                <input @input="chkAddress" readonly type="text" name="addr" id="addr" autocomplete='off' v-model="address" @click="kakaoPostcode" class="admin_user_info10">
+                <label for="det_addr" class="admin_user_info11">상세 주소</label>
+                <input type="text" name="det_addr" id="det_addr" autocomplete='off' v-model="detAddr" class="admin_user_info12">
+                <label for="post" class="admin_user_info13">우편번호</label>
+                <input readonly type="text" name="post" id="post" autocomplete='off' v-model="post" class="admin_user_info14">
+                <button type="button" @click="kakaoPostcode" id="postcode" class="admin_btn admin_user_info15">주소검색</button>
+
+                <label for="birth" class="admin_user_info16">생년월일</label>
                 <p class="admin_error">{{ birthError }}</p>
-                <input @input="chkBirth" type="date" name="birth" id="birth" autocomplete='off' v-model="birth" class="admin_user_info13 admin_user_date">
+                <input @input="chkBirth" type="date" name="birth" id="birth" autocomplete='off' v-model="birth" class="admin_user_info17 admin_user_date">
             </div>
 
             <div class="admin_hr"></div>
@@ -44,6 +52,7 @@ const store = useStore();
 
 // 실시간 유효성 체크
 const name = ref(store.state.adminUserToUpdate.name);
+const password = ref('');
 const email = ref(store.state.adminUserToUpdate.email);
 const tel = ref(store.state.adminUserToUpdate.tel);
 const address = ref(store.state.adminUserToUpdate.addr);
@@ -52,11 +61,22 @@ const post = ref(store.state.adminUserToUpdate.post);
 const birth = ref(store.state.adminUserToUpdate.birth);
 const emailText = ref(store.state.adminUserToUpdate.email);
 
+const passwordError = ref('');
 const emailError = ref('');
 const nameError = ref('');
 const phoneError = ref('');
 const addressError = ref('');
 const birthError = ref('');
+
+function chkPassword() {
+  if (!password.value || password.value.length < 4 || password.value.length > 5) {
+    passwordError.value = '비밀번호는 숫자 4~5자리로 설정 해주세요.';
+  } else if (/\s/.test(password.value)) {
+    passwordError.value = '비밀번호에는 공백을 포함할 수 없습니다.';
+  } else {
+    passwordError.value = '';
+  }
+}
 
 function chkEmail() {
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
